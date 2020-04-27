790f80d70daf29a9c0d53df28aafa71e5c277b9c
x-row <mallumvss@gmail.com>
2020-04-28 00:31:47 +0530

    first commit

---
+++ helpers/downloader.py
@@ -0,0 +1,49 @@
+import time, traceback
+
+class Downloader:
+
+    def __init__(self, m):
+        self.m = m
+
+
+    async def start(self, progress=None, *args):
+        self.callback = progress
+        self.args = args
+
+        await self._download()
+
+        return self.status, self.message
+
+
+    async def _download(self):
+        try:
+            self.start_time = time.time()
+            
+            self.downloaded_file = await self.m.reply_to_message.download(progress = self._callback)
+
+            if(not self.downloaded_file):
+                self.status = False
+                self.message = "Download failed!"
+            else:
+                self.status = True
+                self.message = self.downloaded_file
+
+        except Exception as e:
+            traceback.print_exc()
+            self.status = False
+            self.message = f"Error occuered during download.\nError details: {e}"
+
+        return
+
+
+    async def _callback(self, cur, tot):
+        try:
+            if(not self.callback):
+                return
+            
+            await self.callback(cur, tot, self.start_time, "Downloading...", *self.args)
+
+        except:
+            pass
+
+        return
---
+++ helpers/uploader.py
@@ -0,0 +1,67 @@
+from youtube_upload.auth import GoogleAuth
+from youtube_upload.youtube import Youtube
+
+from config import Config
+
+from translations import Messages as tr
+
+import os
+import time
+import traceback
+
+class Uploader:
+
+    def __init__(self, file, title=None):
+        
+        self.file = file
+        
+        self.title = title
+
+
+    async def start(self, progress=None, *args):
+        self.progress = progress
+        self.args = args
+
+        await self._upload()
+
+        return self.status, self.message
+
+
+    async def _upload(self):
+        try:
+
+            auth = GoogleAuth(Config.CLIENT_ID, Config.CLIENT_SECRET)
+            
+            if not os.path.isfile(Config.CRED_FILE):
+                self.status = False
+                
+                self.message = "Upload failed because you did not authenticate me."
+                
+                return
+
+            auth.LoadCredentialsFile(Config.CRED_FILE)
+
+            google = auth.authorize()
+
+            properties = dict(
+                title = self.title if self.title else os.path.basename(self.file),
+                description = 'Uploaded to youtube with https://t.me/youtubeitbot',
+                category = 27,
+                privacyStatus = 'private'
+            )
+
+            youtube = Youtube(google)
+
+            self.start_time = time.time()
+            self.last_time = self.start_time
+
+            r = await youtube.upload_video(video = self.file, properties = properties)
+
+            self.status = True
+            self.message = f"https://youtu.be/{r['id']}"
+        except Exception as e:
+            traceback.print_exc()
+            self.status = False
+            self.message = f"Error occuered during upload.\nError details: {e}"
+        return
+
